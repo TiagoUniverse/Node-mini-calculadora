35a64dfd76b91726e60b1aa6bc29cf41e5e80fde
TiagoUniverse <tiagocesar68@gmail.com>
2023-09-28 17:06:09 -0300

    Initial commit

---
+++ calc.js
@@ -0,0 +1,10 @@
+var SomaFunc = require("./funcoes/div.js");
+var multiFunc = require("./funcoes/multi.js");
+var somarFunc = require("./funcoes/somar.js");
+var subtrairFunc = require("./funcoes/subtrair");
+
+ console.log(subtrairFunc(2,4));
+
+
+
+ 
